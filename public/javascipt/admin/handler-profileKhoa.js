0de3f44d549121681163f26b592cb63596549cd0
HaTrang96 <hatrang1921@gmail.com>
2016-12-05 08:21:16 +0700

    Update chức năng admin

--- public/javascipt/admin/handler-profileKhoa.js
+++ public/javascipt/admin/handler-profileKhoa.js
@@ -1,26 +1,22 @@
-  function submitButtonHandler(evt) {
-      var idButtonNew = {
-        id : evt.id
-      }
-      //Tao hieu ung xoay tron
 
+function submitButtonHandler(evt) {
+    //Tao hieu ung xoay tron
+    //Sinh đoạn html
+    var data = "";
+    switch (evt.id) {
+        case "tongQuan":
+            $('#showContentHere').load("");
+            break;
+        case "khoiTaoSV":
+            $('#showContentHere').load("../../html/khoa/string-khoiTaoSV.html");
+            break;
+        case "khoiTaoGV":
+            $('#showContentHere').load("../../html/khoa/string-khoiTaoGV.html");
+            break;
+        case "DKKL" :
+           $('#showContentHere').load("");
+            break;
 
-      //make the AJAX call
-      $.ajax({
-        url: '/handlerRederAdmin',
-        type: 'POST',
-        contentType: "application/json",
-        data: JSON.stringify(idButtonNew),
-        success: postSuccessHandler
-      });
-  }
-
-  function postSuccessHandler (htmlData) {
-    var $data = $('#showContentHere');
-    $data.html('');
-
-    //update the UI with the data returned from the AJAX call
-    $.each(htmlData, function (key, val) {
-      $data.append(htmlData.htmlStr);
-    });
-  };
+        default:
+    }
+}
